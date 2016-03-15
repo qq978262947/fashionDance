72fb623396b06c5ff95689c4741606801d6422f3
Dylan <dylancccq@qq.com>
2016-03-15 11:52:49 +0800

    chart

---
+++ fashionDance/fashionDance/classes/FindCar-找车/view/TSPriceChoseView.h
@@ -0,0 +1,17 @@
+//
+//  TSPriceChoseView.h
+//  fashionDance
+//
+//  Created by Dylan on 3/15/16.
+//  Copyright © 2016 汪俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSPriceChoseView : UIView
+//左侧刻度，右侧刻度
+@property (nonatomic,strong)NSNumber * smallNumber;
+@property (nonatomic,strong)NSNumber * bigNumber;
+
+
+@end
