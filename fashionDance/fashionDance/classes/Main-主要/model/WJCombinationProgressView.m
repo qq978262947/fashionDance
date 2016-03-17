6ba619792776880f01ef680a03399278ba4be848
wangjun <978262947@qq.com>
2016-03-17 17:51:24 +0800

    口碑界面基本完成

---
+++ fashionDance/fashionDance/classes/Main-主要/model/WJCombinationProgressView.m
@@ -0,0 +1,21 @@
+//
+//  WJCombinationProgressView.m
+//  fashionDance
+//
+//  Created by 汪俊 on 16/3/17.
+//  Copyright © 2016年 汪俊. All rights reserved.
+//
+
+#import "WJCombinationProgressView.h"
+
+@implementation WJCombinationProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
